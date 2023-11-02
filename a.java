3f3c518c85e9b140bd689c9bc8fa5678972a0b22
Shreshtha Kochgaway <skochgaway112@gmail.com>
2023-11-02 17:51:33 +0530

    while loop

--- a.java
+++ a.java
@@ -1,5 +1,8 @@
     class Main{
         public static void main(String[] args){
-            System.out.println("Hi Shreshtha, first time learning java");
+           for(int i=0;i<4;i++){
+            System.out.print("Hello " );
+            System.out.println(i);
+           }
         }
 }
